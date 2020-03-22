557d96b89987716cfea708e27327d699b2f736a9
anabneri <ananeri32@hotmail.com>
2020-03-22 17:36:32 -0300

    :rocket: adicionando algumas variaveis de ambiente

---
+++ setupAna.sh
@@ -0,0 +1,65 @@
+
+#!/bin/sh
+
+
+
+udo apt-get update
+
+echo 'instalando  curl' 
+sudo apt install curl -y
+
+echo 'instalando Git git' 
+sudo apt install git -y
+
+echo "Qual usuario você usa no Git user.name"
+echo "For example, mine will be \"Erick Wendel\""
+read git_config_user_name
+git config --global user.name "$git_config_user_name"
+clear 
+
+echo "Qual email você usa no Git user.email"
+echo "For example, mine will be \"erick.workspace@gmail.com\""
+read git_config_user_email
+git config --global user.email $git_config_user_email
+clear
+
+echo "Can I set VIM as your default GIT editor for you? (y/n)"
+read git_core_editor_to_vim
+if echo "$git_core_editor_to_vim" | grep -iq "^y" ;then
+	git config --global core.editor vim
+else
+	echo "Okay, no problem. :) Let's move on!"
+fi
+
+echo "Generating a SSH Key"
+ssh-keygen -t rsa -b 4096 -C $git_config_user_email
+ssh-add ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub | xclip -selection clipboard
+
+echo 'installing brave'
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+source /etc/os-release
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ $UBUNTU_CODENAME main" | sudo tee /etc/apt/sources.list.d/brave-browser-release-${UBUNTU_CODENAME}.list
+sudo apt update
+sudo apt install brave-keyring brave-browser
+
+echo 'installing dbeaver'
+wget -c https://dbeaver.io/files/6.0.0/dbeaver-ce_6.0.0_amd64.deb
+sudo dpkg -i dbeaver-ce_6.0.0_amd64.deb
+sudo apt-get install -f
+
+echo 'instalando o java'
+sudo add-apt-repository ppa:webupd8team/java.
+sudo apt-get update.
+sudo apt-get install oracle-java8-installer
+
+
+
+
+
+
+
+
+
+
+
